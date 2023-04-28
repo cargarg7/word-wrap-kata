04c93983236cafd39e9a3f35a3dca78e60255e46
cargarg7 <cargarg7@gmail.com>
2023-04-28 11:46:10 +0200

    red: Should return 1 when input is 1

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,5 +5,9 @@ import { fibonacci } from './index';
 describe('Fibonacci', () => {
     it('Should return 0 when input is 0', () => {
         expect(fibonacci(0)).toBe(0);
-    })
+    });
+
+    it('Should return 1 when input is 1', () => {
+        expect(fibonacci(1)).toBe(1);
+    });
 })
